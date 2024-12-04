7d7aeda339caef48b64fca57a430c308d7410c43
Zied-BenBahri <ziedbenbahri@gmail.com>
2024-12-04 23:33:55 +0100

    -

--- enquete/views.py
+++ enquete/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import *
 from django.core.paginator import Paginator
-# Create your views here.
+
 
 def enquete_map(request):
     if not(request.user.is_authenticated):
